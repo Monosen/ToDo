fd87fc2060d376fbb8da173596c42dc70a5809a6
edinson bolanos <edisonbj21@gmail.com>
2021-11-03 20:03:03 -0500

    new desing

--- src/Components/Home/Items.jsx
+++ src/Components/Home/Items.jsx
@@ -10,11 +10,11 @@ const Items = ({
 	handleItemListDelete,
 }) => {
 	return (
-		<div className="w-80 mx-4 my-3 border border-gray-300 shadow rounded px-3.5 py-4 h-36 transition transform hover:-translate-y-2 hover:translate-x-2 hover:shadow-xl relative">
+		<div className="w-80 mx-4 my-3 border border-gray-300 shadow rounded px-3.5 py-4 h-36 transition transform hover:-translate-y-2 hover:translate-x-2 hover:shadow-xl relative flex flex-col justify-between">
 			<p className="mb-3">{title}</p>
 			<button
 				onClick={() => handleAllListComplete(id)}
-				className="px-5 py-3 bg-black text-white rounded capitalize mt-auto hover:text-black hover:bg-white border-2 border-black font-medium"
+				className="px-5 py-3 bg-black text-white rounded capitalize mt-auto hover:text-black hover:bg-white border-2 border-black font-medium w-32"
 			>
 				completar
 			</button>
